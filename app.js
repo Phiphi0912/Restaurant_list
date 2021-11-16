d0497e5759d094bffcfda5533d165d44b90be2a5
phiphi0912 <610733005@gms.ndhu.edu.tw>
2021-11-16 14:13:56 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json').results
 const mongoose = require('mongoose')
 const db = mongoose.connection
+const Restaurant = require('./models/restaurant')
 
 const app = express()
 const port = 3000
@@ -24,8 +25,12 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurantList })
+  Restaurant.find()
+    .lean()
+    .then((restaurants) => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
 })
+
 
 app.get('/restaurant/:id', (req, res) => {
   const restaurant = restaurantList.find((item) => item.id === Number(req.params.id))
