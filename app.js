fb1df2072ba53403af385edbf9559f899c1b6167
phiphi0912 <610733005@gms.ndhu.edu.tw>
2021-11-17 11:14:01 +0800

    feat: add delete function

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post('/restaurant/create', (req, res) => {
 app.get('/restaurant/:id/detail', (req, res) => {
   return Restaurant.findById(req.params.id)
     .lean()
-    .then((restaurants) => res.render('show', { restaurants }))
+    .then((restaurant) => res.render('show', { restaurant }))
     .catch(error => console.log(error))
 })
 
@@ -57,8 +57,17 @@ app.get('/restaurant/:id/edit', (req, res) => {
 })
 
 app.post('/restaurant/:id/edit', (req, res) => {
-  return Restaurant.findByIdAndUpdate(req.params.id, req.body)
+  return Restaurant.findByIdAndUpdate(req.params.id, req.body)  //前面放篩選條件，後面放更新物件
     .then(() => res.redirect(`/restaurant/${req.params.id}/detail`))
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurant/:id/delete', (req, res) => {
+  if (!req.params.id) return
+
+  return Restaurant.findById(req.params.id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
@@ -66,14 +75,16 @@ app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim().toLowerCase()
   Restaurant.find({
     $or: [  //這裡使用mongoose提供的or 運算子
-      { name: { $regex: keyword } }, //這裡使用mongoose提供的正規表達式來搜尋
-      { category: { $regex: keyword } }
+      { name: { $regex: keyword, $options: 'i' } }, //這裡使用mongodb提供使用正則表達式$regex操作符來進行搜尋
+      { category: { $regex: keyword, $options: 'i' } } //options則可以用來使用其他的表達是，i則代表不分大小寫
     ]
   })
     .lean()
     .then((restaurants) => res.render('index', { restaurants, keyword }))
     .catch(error => console.log(error))
 })
+
+
 
 app.listen(port, () => {
   console.log(`Running on the http://localhost:${port}`)
