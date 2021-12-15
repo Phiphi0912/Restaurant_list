71cf45f2aa079ef5fcacbfcd1a21598b31502e93
phiphi0912 <610733005@gms.ndhu.edu.tw>
2021-12-15 20:57:03 +0800

    feat: modify sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -11,12 +11,21 @@ router.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
-router.get('/sort', (req, res) => {
-  
-})
-
 router.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim().toLowerCase()
+
+  Restaurant.find({
+    $or: [     //這裡使用MongoDB提供的or運算子：{ $or: [ { <expression1> }, { <expression2> }, ... , { <expressionN> } ] }
+      { name: { $regex: keyword, $options: 'i' } }, //這裡使用mongodb提供使用正則表達式$regex操作符來進行搜尋
+      { category: { $regex: keyword, $options: 'i' } } //options則可以用來使用其他的表達是，i則代表不分大小寫
+    ]
+  })
+    .lean()
+    .then((restaurants) => res.render('index', { restaurants, keyword }))
+    .catch(error => console.log(error))
+})
+
+router.get('/sort', (req, res) => {
   const sortSelection = req.query.sort
   const sortMethod = {}
 
@@ -34,16 +43,10 @@ router.get('/search', (req, res) => {
       sortMethod.location = 'asc'
       break
   }
-
-  Restaurant.find({
-    $or: [     //這裡使用MongoDB提供的or運算子：{ $or: [ { <expression1> }, { <expression2> }, ... , { <expressionN> } ] }
-      { name: { $regex: keyword, $options: 'i' } }, //這裡使用mongodb提供使用正則表達式$regex操作符來進行搜尋
-      { category: { $regex: keyword, $options: 'i' } } //options則可以用來使用其他的表達是，i則代表不分大小寫
-    ]
-  })
+  Restaurant.find()
     .lean()
     .sort(sortMethod)
-    .then((restaurants) => res.render('index', { restaurants, keyword, sortSelection }))
+    .then((restaurants) => res.render('index', { restaurants, sortSelection }))
     .catch(error => console.log(error))
 })
 
